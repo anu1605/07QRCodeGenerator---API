7880c12949e830638ed8308696052ef256a45e49
Anushka Tyagi <anushka.tyagi161998@gmail.com>
2023-02-23 18:34:54 +0530

    final

--- App.js
+++ App.js
@@ -2,11 +2,11 @@ var submit = document.getElementById('submit');
 var input = document.getElementById('input');
 var qrCode;
 
-submit.addEventListener('click', generateQR(input.value));
+
 
 function generateQR(content) {
     return new QRCode(
-        document.getElementById('img'), {
+        document.getElementById('main'), {
             text: content,
             width: 256,
             height: 256,
@@ -27,11 +27,6 @@ submit.addEventListener("click", function (event) {
         qrCode.makeCode(qrContent);
     }
 
-    console.log(document.getElementById('img'));
-    var source = qrCode._el.childNodes[3].currentSrc;
-    console.log(qrCode._el.childNodes,source);
-    document.getElementById('img').src= source;
-    document.getElementById('img').style.display = 'block';
 });
 
 
